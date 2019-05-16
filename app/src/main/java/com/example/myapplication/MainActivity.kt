1349c10ec6ba9c5626c1cf887f8e1c3bfe1cc4a4
lee6265 <tllee@cute.edu.tw>
2019-05-16 15:50:31 +0800

    Add button clicklistener

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val message: TextView=findViewById(R.id.txtName)
+        var btnOK: Button =findViewById(R.id.btnOK)
+        btnOK.setOnClickListener(View.OnClickListener {
+            message.setText("測試資料")
+        })
+
     }
 }
